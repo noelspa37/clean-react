a9e38781f5aee99a08517e0915a61bc92fd3d776
noelspa37 <noel.platzi.spa.37@gmail.com>
2020-08-02 16:42:24 -0500

    refactor: move logo to a separate file

---
+++ src/presentation/components/logo/logo.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Logo: React.FC = () => {
+  return (
+    <input type='email' name='email' placeholder='Digite su email' />
+  )
+}
+
+export default Logo
--- src/presentation/pages/login/login.tsx
+++ src/presentation/pages/login/login.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import Styles from './login-styles.scss'
 import Spinner from '@/presentation/components/spinner/spinner'
+import Logo from '@/presentation/components/logo/logo'
 
 const Login: React.FC = () => {
   return (
@@ -12,7 +13,7 @@ const Login: React.FC = () => {
       <form className={Styles.form} >
         <h2>Login</h2>
         <div className={Styles.inputWrap}>
-          <input type='email' name='email' placeholder='Digite su email' />
+          <Logo />
           <span className={Styles.status} >:(</span>
         </div>
         <div className={Styles.inputWrap}>
