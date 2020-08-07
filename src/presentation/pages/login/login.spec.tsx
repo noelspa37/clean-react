e53178fa19a968a2ada88b0ea950930cafc9f06a
noelspa37 <noel.platzi.spa.37@gmail.com>
2020-08-06 21:03:05 -0500

    test: ensure Login disable submit button only if form is invalid

--- src/presentation/pages/login/login.spec.tsx
+++ src/presentation/pages/login/login.spec.tsx
@@ -70,4 +70,15 @@ describe('Login Component', () => {
     expect(passwordStatus.title).toBe('Todo cierto')
     expect(passwordStatus.textContent).toBe(':)')
   })
+
+  test('Should enable submit button if form is valid ', () => {
+    const { sut, validationStub } = makeSut()
+    validationStub.errorMessage = null
+    const emailInput = sut.getByTestId('email')
+    fireEvent.input(emailInput, { target: { value: faker.internet.email() } })
+    const passwordInput = sut.getByTestId('password')
+    fireEvent.input(passwordInput, { target: { value: faker.internet.password() } })
+    const submitButton = sut.getByTestId('submit') as HTMLButtonElement
+    expect(submitButton.disabled).toBe(false)
+  })
 })
