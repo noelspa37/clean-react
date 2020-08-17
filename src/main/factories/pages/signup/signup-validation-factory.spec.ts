2fe4bf9c14f010466fcc8ef663628329737061d3
noelspa37 <noel.platzi.spa.37@gmail.com>
2020-08-16 22:06:59 -0500

    test: add SignUpValidation factory

---
+++ src/main/factories/pages/signup/signup-validation-factory.spec.ts
@@ -0,0 +1,15 @@
+import { makeSignUpValidation } from './signup-validation-factory'
+import { ValidationBuilder as Builder } from '@/validation/validators/builder/validation-builder'
+import { ValidationComposite } from '@/validation/validators'
+
+describe('SignUpValidationFactory', () => {
+  test('Should composite ValidationComposite with correct values ', () => {
+    const composite = makeSignUpValidation()
+    expect(composite).toEqual(ValidationComposite.build([
+      ...Builder.field('name').required().min(5).build(),
+      ...Builder.field('email').required().email().build(),
+      ...Builder.field('password').required().min(5).build(),
+      ...Builder.field('passwordConfirmation').required().min(5).build()
+    ]))
+  })
+})
