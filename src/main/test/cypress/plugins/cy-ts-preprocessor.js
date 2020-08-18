6fe283322c93c465208874896ac93203f8f33dee
noelspa37 <noel.platzi.spa.37@gmail.com>
2020-08-17 22:56:43 -0500

    fix: ignore cypress folder for tsconfig

--- src/main/test/cypress/plugins/cy-ts-preprocessor.js
+++ src/main/test/cypress/plugins/cy-ts-preprocessor.js
@@ -1,16 +1,16 @@
 const wp = require('@cypress/webpack-preprocessor')
 
 module.exports = wp({
-    webpackOptions: {
-        resolve: {
-            extensions: ['.ts', '.js']
-        },
-        module: {
-            rules: [{
-                test: /\.ts$/,
-                exclude: /node_modules/,
-                loader: 'ts-loader'
-            }]
-        }
+  webpackOptions: {
+    resolve: {
+      extensions: ['.ts', '.js']
+    },
+    module: {
+      rules: [{
+        test: /\.ts$/,
+        exclude: /node_modules/,
+        loader: 'ts-loader'
+      }]
     }
+  }
 })
