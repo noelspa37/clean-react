6a4d96423f4c1ea07d67e9c3fc2fa37b57c1fe72
noelspa37 <noel.platzi.spa.37@gmail.com>
2020-08-21 16:41:56 -0500

    test: ensure SignUp present valid state if form is valid

--- src/main/test/cypress/integration/signup.spec.ts
+++ src/main/test/cypress/integration/signup.spec.ts
@@ -20,7 +20,7 @@ describe('SignUp', () => {
   })
 
   it('Should present error state if form is invalid', () => {
-    cy.getByTestId('name').focus().type(faker.random.alphaNumeric(3))
+    cy.getByTestId('name').focus().type(faker.random.alphaNumeric(4))
     FormHelper.testInputStatus('name', 'Valor inválido')
     cy.getByTestId('email').focus().type(faker.random.word())
     FormHelper.testInputStatus('email', 'Valor inválido')
@@ -29,6 +29,20 @@ describe('SignUp', () => {
     cy.getByTestId('passwordConfirmation').focus().type(faker.random.alphaNumeric(3))
     FormHelper.testInputStatus('passwordConfirmation', 'Valor inválido')
     cy.getByTestId('submit').should('have.attr', 'disabled')
+    cy.getByTestId('error-wrap').should('not.have.descendants')
+  })
+
+  it('Should present valid state if form is valid', () => {
+    cy.getByTestId('name').focus().type(faker.name.findName())
+    FormHelper.testInputStatus('name')
+    cy.getByTestId('email').focus().type(faker.internet.email())
+    FormHelper.testInputStatus('email')
+    const password = faker.random.alphaNumeric(5)
+    cy.getByTestId('password').focus().type(password)
+    FormHelper.testInputStatus('password')
+    cy.getByTestId('passwordConfirmation').focus().type(password)
+    FormHelper.testInputStatus('passwordConfirmation')
+    cy.getByTestId('submit').should('not.have.attr', 'disabled')
     cy.getByTestId('error-wrap').should('not.have.descendants')
   })
 })
