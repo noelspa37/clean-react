e6e58b444056c1992fb3646116da8c8e77f5f05e
noelspa37 <noel.platzi.spa.37@gmail.com>
2020-09-29 20:31:14 -0500

    refactor: change validation factory not use builder

--- src/main/factories/pages/signup/signup-validation-factory.spec.ts
+++ src/main/factories/pages/signup/signup-validation-factory.spec.ts
@@ -1,15 +1,18 @@
 import { makeSignUpValidation } from './signup-validation-factory'
-import { ValidationBuilder as Builder } from '@/validation/validators/builder/validation-builder'
-import { ValidationComposite } from '@/validation/validators'
+import { EmailValidation, MinLengthValidation, RequiredFieldValidation, ValidationComposite, CompareFieldsValidation } from '@/validation/validators'
 
 describe('SignUpValidationFactory', () => {
   test('Should composite ValidationComposite with correct values ', () => {
     const composite = makeSignUpValidation()
     expect(composite).toEqual(ValidationComposite.build([
-      ...Builder.field('name').required().min(5).build(),
-      ...Builder.field('email').required().email().build(),
-      ...Builder.field('password').required().min(5).build(),
-      ...Builder.field('passwordConfirmation').required().sameAs('password').build()
+      new RequiredFieldValidation('name'),
+      new MinLengthValidation('name', 5),
+      new RequiredFieldValidation('email'),
+      new EmailValidation('email'),
+      new RequiredFieldValidation('password'),
+      new MinLengthValidation('password', 5),
+      new RequiredFieldValidation('passwordConfirmation'),
+      new CompareFieldsValidation('passwordConfirmation', 'password')
     ]))
   })
 })
